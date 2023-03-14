3bdc3f27eb394faad241ef4a1fe8e4a00eac7458
mjx <1990824801@qq.com>
2023-03-14 20:13:50 +0800

    Initial commit

---
+++ src/main/java/com/atguogu/boot/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguogu.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+   @RequestMapping("hello")
+    public String handle(){
+       return "Hello Spring Boot";
+   }
+
+}
